083c60366fbe06ea20705b1e2c1dc3dc664b7068
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2022-11-27 19:49:01 +0100

    api-key auth middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { config as dotenv } from 'dotenv';
 import express, { Express, NextFunction, Request, Response } from 'express';
 import { ApiError, errorConverter, errorHandler } from './middlewares/error-handler';
+import { apiKeyAuth } from './middlewares/api-key-auth';
+import { name, version } from '../package.json';
 
 dotenv({ path: path.resolve(process.cwd(), '.env.local') });
 dotenv({ path: path.resolve(process.cwd(), '.env') });
@@ -10,9 +12,19 @@ const app: Express = express();
 
 app.use(express.json());
 
-app.use((req: Request, _: Response, next: NextFunction) => next(new ApiError(404, `Page ${req.path} not found`)));
+app.get('/', (_: Request, res: Response) => {
+  res.status(200).json({ app: name, version });
+});
+
+app.use(apiKeyAuth);
+
+app.use((req: Request, _: Response, next: NextFunction) => {
+  next(new ApiError(404, `Page ${req.path} not found`));
+});
 
 app.use(errorConverter);
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('Docker Webhook is working and listening on http://0.0.0.0:3000'));
+app.listen(3000, () => {
+  console.log('Docker Webhook is working and listening on http://0.0.0.0:3000');
+});
---
+++ src/middlewares/api-key-auth/index.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+import { ApiError } from '../error-handler';
+
+export const apiKeyAuth = (req: Request, _: Response, next: NextFunction) => {
+  const valid = process.env['API_KEY'];
+  const key = req.headers['x-api-key'];
+  if (key === undefined) {
+    throw new ApiError(401, 'No API key');
+  }
+  if (key !== valid) {
+    throw new ApiError(403, 'API key invalid');
+  }
+  next();
+};
